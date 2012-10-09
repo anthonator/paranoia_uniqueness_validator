0c6f8aca39451f1659008269a26e1722387f65e1
Anthony Smith <anthony@sticksnleaves.com>
2012-10-09 18:11:44 -0400

    Added validates_uniqueness_without_deleted method.

--- lib/paranoia_uniqueness_validator.rb
+++ lib/paranoia_uniqueness_validator.rb
@@ -1,5 +1,10 @@
+require "paranoia_uniqueness_validator/validations/uniqueness_without_deleted"
 require "paranoia_uniqueness_validator/version"
 
 module ParanoiaUniquenessValidator
-  # Your code goes here...
+
+end
+
+class ActiveRecord::Base
+  include ParanoiaUniquenessValidator::Validations
 end
--- lib/paranoia_uniqueness_validator/validations/uniqueness_without_deleted.rb
+++ lib/paranoia_uniqueness_validator/validations/uniqueness_without_deleted.rb
@@ -24,11 +24,11 @@ module ParanoiaUniquenessValidator
           record.errors.add(attribute, :taken, options.except(:case_sensitive, :scope).merge(:value => value))
         end
       end
+    end
 
-      module ClassMethods
-        def validates_uniqueness_without_deleted_of(*attr_name)
-          validates_with UniquenessWithoutDeletedValidator, _merge_attributes(attr_names)
-        end
+    module ClassMethods
+      def validates_uniqueness_without_deleted_of(*attr_name)
+        validates_with UniquenessWithoutDeletedValidator, _merge_attributes(attr_names)
       end
     end
   end
