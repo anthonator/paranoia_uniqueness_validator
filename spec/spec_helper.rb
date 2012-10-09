dc072a48165b08ae44e64c9918d6d3c8b61d62b9
Anthony Smith <anthony@sticksnleaves.com>
2012-10-09 17:59:45 -0400

    Configured rspec.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,33 @@
+ENV["RAILS_ENV"] = 'test'
+
+# Require dummy Rails app
+require File.expand_path("../../spec/dummy/config/environment", __FILE__)
+
+require 'database_cleaner'
+require 'rspec/rails'
+require 'rspec/autorun'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+DatabaseCleaner.strategy = :transaction
+
+RSpec.configure do |config|
+  # If true, the base class of anonymous controllers will be inferred
+  # automatically. This will be the default behavior in future versions of
+  # rspec-rails.
+  config.infer_base_class_for_anonymous_controllers = false
+
+  config.before :suite do
+    DatabaseCleaner.clean_with :truncation
+  end
+
+  config.before :each do
+    DatabaseCleaner.start
+  end
+
+  config.after :each do
+    DatabaseCleaner.clean
+  end
+end
