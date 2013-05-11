98fd6156a0d13e16af19b6d4a17f7601f2f82310
Anthony Smith <anthony@sticksnleaves.com>
2013-05-11 04:12:47 -0400

    Fixed syntax errors.

--- spec/dummy/config/boot.rb
+++ spec/dummy/config/boot.rb
@@ -4,4 +4,4 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
-$:unshift File.expand_path('../../../../lib' __FILE__)
+$:.unshift File.expand_path('../../../../lib', __FILE__)
