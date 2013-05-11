be83aa5b9c195830a82e06b1679ae7ca9f9796f4
Anthony Smith <anthony@sticksnleaves.com>
2013-05-11 04:10:34 -0400

    Configured dummy app.

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -10,6 +10,7 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
+require "paranoia_uniqueness_validator"
 
 module Dummy
   class Application < Rails::Application
