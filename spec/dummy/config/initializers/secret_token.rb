e8ea4dbc3a618878c84766e6d0374d8b8014a375
Anthony Smith <anthony@sticksnleaves.com>
2013-05-11 04:01:34 -0400

    Removed Rails 3 dummy app.

--- spec/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'a9dd301308a3e1d55e67e49f6fefdcd9c82b426d7315fab80885a40272d687ea0e0dd8727f5ba3b71e477353436a7dd80e0026ea518986f949fd355fdff7065d'
--- spec/dummy/db/migrate/20121009200838_create_dummy_models.rb
+++
@@ -1,9 +0,0 @@
-class CreateDummyModels < ActiveRecord::Migration
-  def change
-    create_table :dummy_models do |t|
-      t.string   :unique_field
-      t.datetime :deleted_at
-      t.timestamps
-    end
-  end
-end
